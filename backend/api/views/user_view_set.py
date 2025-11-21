a2207db3099444763ef0c13ec8ae8b3ad34c1610
vo-olivier250405 <oliviervo.pro@gmail.com>
2025-11-21 21:34:11 +0100

    [ADD] => /me endpoint

--- backend/api/views/user_view_set.py
+++ backend/api/views/user_view_set.py
@@ -31,3 +31,8 @@ class UserViewSet(BaseViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=["get"])
+    def me(self, request):
+        serializer = self.get_serializer_class()(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
