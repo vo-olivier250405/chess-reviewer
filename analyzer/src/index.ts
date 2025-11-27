f7e83a0cc4f1894668f896f87732d8459fe624f0
vo-olivier250405 <oliviervo.pro@gmail.com>
2025-11-27 22:14:10 +0100

    [ADD] => config cors

--- analyzer/src/index.ts
+++ analyzer/src/index.ts
@@ -1,7 +1,18 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import apiRoute from "./routes";
 
 const app = new Hono();
+
+app.use(
+  "/*",
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:8000"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.get("/", (c) => c.text("Hello from The Chess Review !"));
 
